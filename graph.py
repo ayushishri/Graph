7939bc9c515ecaa130a28cc56555a4ec0af399bd
ayushishri <32212671+ayushishri@users.noreply.github.com>
2018-09-07 15:09:35 -0400

    Graph

--- graph.py
+++ graph.py
@@ -1,15 +1,9 @@
 import sys
-sys.path.append(r'C:\Users\ayush\Documents\GitHub\Stack')
-sys.path.append(r'C:\Users\ayush\Documents\GitHub\Queue')
+sys.path.append(r'C:\Users\ayush\Documents\GitHub\Stack-Queue')
 
 #the 'r' converts normal string to raw string
 #otherwise you will get the error "(unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape"
 
-from stack import stack
-from queue import *
+from stackqueue import *
 
 
-a=queue.queue()
-a.enQ(3)
-a.enQ(4)
-print(a)
